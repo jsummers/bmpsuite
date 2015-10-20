ae7792ed6f8b9e6df27221b7598f1e057b1c0155
Jason Summers <jason1@pobox.com>
2015-10-20 13:26:34 -0400

    Added files q/rgb32h52.bmp and q/rgba32h56.bmp

--- bmpsuite.c
+++ bmpsuite.c
@@ -1742,6 +1742,31 @@ static int run(struct context *c)
 	if(!make_bmp_file(c)) goto done;
 
 	defaultbmp(c);
+	c->filename = "q/rgb32h52.bmp";
+	c->headersize = 52;
+	c->bpp = 32;
+	c->compression = BI_BITFIELDS;
+	c->bf_r = 0xff000000; c->nbits_r = 8; c->bf_shift_r = 24;
+	c->bf_g = 0x0000ff00; c->nbits_g = 8; c->bf_shift_g = 8;
+	c->bf_b = 0x000000ff; c->nbits_b = 8; c->bf_shift_b = 0;
+	c->pal_entries = 0;
+	set_calculated_fields(c);
+	if(!make_bmp_file(c)) goto done;
+
+	defaultbmp(c);
+	c->filename = "q/rgba32h56.bmp";
+	c->headersize = 56;
+	c->bpp = 32;
+	c->compression = BI_BITFIELDS;
+	c->bf_r = 0xff000000; c->nbits_r = 8; c->bf_shift_r = 24;
+	c->bf_g = 0x0000ff00; c->nbits_g = 8; c->bf_shift_g = 8;
+	c->bf_b = 0x000000ff; c->nbits_b = 8; c->bf_shift_b = 0;
+	c->bf_a = 0x00ff0000; c->nbits_a = 8; c->bf_shift_a = 16;
+	c->pal_entries = 0;
+	set_calculated_fields(c);
+	if(!make_bmp_file(c)) goto done;
+
+	defaultbmp(c);
 	c->filename = "q/rgba32abf.bmp";
 	c->bpp = 32;
 	c->compression = BI_ALPHABITFIELDS;
